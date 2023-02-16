b7bcfff7a9ec2761d0a4791fc99b446b104e09e6
unknown <advaitkr91@gmail.com>
2023-02-16 21:38:59 +0530

    first commit

---
+++ check.js
@@ -0,0 +1,65 @@
+function executeValue(callback,value) { return typeof callback === 'function' ? callback(value) : value }
+function zero(callback) {
+  const value = 0;
+  return executeValue(callback,value);
+}
+function one(callback) {
+  const value = 1;
+  return executeValue(callback,value);
+}
+function two(callback) {
+  const value = 2;
+  return executeValue(callback,value);
+}
+function three(callback) {
+  const value = 3;
+  return executeValue(callback,value);
+}
+function four(callback) {
+  const value = 4;
+  return executeValue(callback,value);
+}
+function five(callback) {
+  const value = 5;
+  return executeValue(callback,value);
+}
+function six(callback) {
+  const value = 6;
+  return executeValue(callback,value);
+}
+function seven(callback) {
+  const value = 7;
+  return executeValue(callback,value);
+}
+function eight(callback) {
+  const value = 8;
+  return executeValue(callback,value);
+}
+function nine(callback) {
+  const value = 9;
+  return executeValue(callback,value);
+}
+
+function plus(a) {
+  return (b) => {
+    return a + b
+  }
+}
+function minus(a) {
+  return (b) => {
+    return b - a
+  }
+}
+function times(a) {
+  return (b) => {
+    return a * b
+  }
+}
+function dividedBy(a) {
+  return (b) => {
+    return Math.floor(b/a)
+  }
+}
+
+let ans = four(plus(nine()))
+console.log(ans)
