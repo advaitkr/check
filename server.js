f0982d6660f06dfa1fe5c9d6578337ee830fd8e1
unknown <advaitkr91@gmail.com>
2023-02-19 03:05:17 +0530

    changes made to deploy on netlify

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose')
+const serverless = require('serverless-http')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 //const dotenv = require('dotenv')
 require('dotenv').config()
 const dbConnect = require("./config/config")
@@ -8,17 +11,21 @@ const PORT = process.env.PORT || 5000
 const {signup,signin,protect,getAll,activity,activitylog,restrictTo} = require("./controllers/userController")
 dbConnect()
 app.use(express.json())
-app.get("/",(req,res)=>{
-    res.send({"msg":"Hello"})
-})
-app.post('/signup',signup)
-app.post('/signin',signin)
-app.get('/getall',protect,getAll)
-app.post('/getresult/:studentId/:masterId',activity)
-app.get('/activitylog',protect,restrictTo('student'),activitylog)
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+const router = express.Router()
+router.post('/signup',signup)
+router.post('/signin',signin)
+router.get('/getall',protect,getAll)
+router.post('/getresult/:studentId/:masterId',activity)
+router.get('/activitylog',protect,restrictTo('student'),activitylog)
+app.use('/.netlify/functions/server', router);
 
+module.exports = app;
+module.exports.handler = serverless(app);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running at Port ${PORT}`)
-  })
+// app.listen(PORT,()=>{
+//     console.log(`Server is running at Port ${PORT}`)
+//   })
